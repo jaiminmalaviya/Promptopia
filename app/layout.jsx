12c655647c6814963405cbaefc6c0fe32777e227
Jaimin Malaviya <Jaiminmalviya56.jm@gmail.com>
2023-10-25 16:56:45 +0530

    import vercel/analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,14 +1,15 @@
 import '@styles/globals.css'
 import Nav from '@components/Nav'
 import Provider from '@components/Provider'
-import {Toaster} from 'react-hot-toast'
+import { Toaster } from 'react-hot-toast'
+import { Analytics } from '@vercel/analytics/react'
 
 export const metadata = {
     title: 'Promptopia',
     description: 'Discover & Share AI Prompts',
 }
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
     return (
         <html lang="en">
             <body>
